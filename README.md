fe8d4f0877dbaf31a3f20ea371853f16a5489bd7
James K Nelson <james@numbat.com.au>
2012-11-20 21:23:11 +0800

    initial version of yaml smart package

---
+++ README.md
@@ -0,0 +1,28 @@
+# YAML
+
+This smart package lets you parse and generate YAML within a meteor app. It will also automatically load all .yaml files in your project, parse them into javascript objects, and make them available at `YAML.data`. 
+
+It is based on [yaml.js](https://github.com/jeremyfa/yaml.js) by jeremyfa.
+
+## Installation
+
+```
+mrt install yaml
+```
+
+## Usage
+
+```javascript
+YAML.parse(text);
+```
+Parses the YAML given by `text` and returns it as a javascript object.
+
+
+```javascript
+YAML.stringify(array, inline, spaces);
+```
+Attempts to return the given array as a yaml string. 
+
+Parameters:
+`inline`: The level where you switch to inline YAML
+`spaces`: The number of spaces used for indentation
---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+  summary: "underscore.string repackaged for Meteor"
+});
+
+var fs = require('fs'),
+	yaml;
+
+// To the gods of meteor - please don't make me do this! :(
+try {
+	yaml = require('../../packages/yaml/yaml.js');
+}
+catch(e) {
+	yaml = require(process.cwd() + "/.meteor/meteorite/packages/yaml/yaml.js");
+}
+
+Package.on_use(function (api, where) {
+  where = where || ['client', 'server'];
+
+  api.use('underscore', where);
+
+  api.add_files('yaml.js', where);
+});
+
+Package.register_extension("yaml", function (bundle, source_path, serve_path, where) {
+    var yaml_path = serve_path.split('.')[0].split('/').splice(1)
+      , contents = fs.readFileSync(source_path)
+      , loader = "YAML._import("+JSON.stringify(yaml_path)+", "+JSON.stringify(yaml.parse(contents.toString('utf8')))+");";
+    contents = new Buffer(loader);
+    serve_path = serve_path + '.js';
+
+    bundle.add_resource({
+      type: "js",
+      path: serve_path,
+      data: contents,
+      where: where
+    });
+  }
+);
